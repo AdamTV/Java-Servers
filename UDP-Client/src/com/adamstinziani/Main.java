ef588538b1b0a1140909d6bc8faec1d4ab95da32
AdamTV <43098728+AdamTV@users.noreply.github.com>
2019-06-06 20:58:23 -0400

    UDP

--- UDP-Client/src/com/adamstinziani/Main.java
+++ UDP-Client/src/com/adamstinziani/Main.java
@@ -1,8 +1,45 @@
 package com.adamstinziani;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+
+/*
+UDP Server
+Made by Adam Stinziani
+Date: 2019-05-06
+ */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            InetAddress address = InetAddress.getLocalHost(); //getByName() pass host name
+            DatagramSocket datagramSocket = new DatagramSocket(); //Isn't associated with a port
+
+            Scanner scanner = new Scanner(System.in);
+            String echoString;
+
+            do{
+                System.out.printf("Enter string to be echoed: ");
+                echoString = scanner.nextLine();
+                byte[] buffer = echoString.getBytes();
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 5000);
+                datagramSocket.send(packet);
+
+                byte[] buffer2 = new byte[50];
+                packet = new DatagramPacket(buffer2,buffer2.length);
+                //Going to wait here until it receives server's message
+                datagramSocket.receive(packet);
+                System.out.println("Text received is: " + new String(buffer2,0,packet.getLength()));
+
+            }while(!echoString.equals("exit"));
+        } catch (SocketTimeoutException e) {
+            System.out.println("The socket timed out");
+        } catch (IOException e) {
+            System.out.println("Client error: " + e.getMessage());
+        }
     }
 }
--- UDP-Server/src/com/adamstinziani/Main.java
+++ UDP-Server/src/com/adamstinziani/Main.java
@@ -1,4 +1,8 @@
 package com.adamstinziani;
+
+import java.io.IOException;
+import java.net.*;
+
 /*
 UDP Server
 Made by Adam Stinziani
@@ -7,6 +11,26 @@ Date: 2019-05-06
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            DatagramSocket socket = new DatagramSocket(5000);
+            while(true){
+                byte[] buffer = new byte[50];
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                //Blocked until packet received (does not create end to end connection)
+                socket.receive(packet);
+                System.out.println("Text received is: " + new String(buffer,0,packet.getLength()));
+
+                String returnString = "echo: " + new String(buffer,0,packet.getLength());
+                byte[] buffer2 = returnString.getBytes();
+                InetAddress address = packet.getAddress();
+                int port = packet.getPort();
+                packet = new DatagramPacket(buffer2, buffer2.length, address, port);
+                socket.send(packet);
+            }
+        } catch (SocketException e) {
+            System.out.println("SocketException: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
     }
 }
